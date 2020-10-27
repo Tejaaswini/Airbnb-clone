11bea15b05d285429ae72f004ac93468403089ba
Tejaaswini <tejnaren07@gmail.com>
2020-10-27 19:51:04 +0530

    update UI changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 //React
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home'
 import Header from './components/Header'
+import SearchPage from './components/SearchPage'
 
 function App() {
   return (
     <div>
-     <Header />
-     <Home />
+     <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </ Router>
     </div>
   );
 }
